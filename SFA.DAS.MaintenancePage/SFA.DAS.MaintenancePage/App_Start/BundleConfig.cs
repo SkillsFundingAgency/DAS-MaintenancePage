17c28d153f67d9d05c7a6f344501039c4b5db970
Simon Bull <simon.bull@taurumconsulting.com>
2016-06-20 15:36:53 +0100

    Basic site

---
+++ SFA.DAS.MaintenancePage/SFA.DAS.MaintenancePage/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Optimization;
+
+namespace SFA.DAS.MaintenancePage
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                "~/scripts/govuk-template.js"));
+
+            bundles.Add(new StyleBundle("~/Content/stylesheets/bundled-css").Include(
+                "~/Content/stylesheets/govuk-template.css",
+                "~/Content/stylesheets/fonts.css",
+                "~/Content/stylesheets/das-controls.css",
+                "~/Content/stylesheets/maintenance-main.css"));
+        }
+    }
+}
